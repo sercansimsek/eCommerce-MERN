b13567b18cd274244a8e7c9dd08ad0b639016b7a
Sercan <sercansimsek44@gmail.com>
2026-08-27 14:18:41 +0200

    basic express sercer created, products server statically, development scripts arranged

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import products from "./data/products.js";
+
+const PORT = 5000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
