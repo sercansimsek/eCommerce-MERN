afe30f57ae967451d00b0173236c6202a98a3cc2
Sercan <sercansimsek44@gmail.com>
2026-08-27 10:32:49 +0200

    product ratings are added to the product card

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,8 +1,9 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Rating } from "./Rating";
 
 export const Product = ({ product }) => {
-  const { image, name, price } = product;
+  const { image, name, price, rating, numReviews } = product;
   return (
     <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
@@ -11,10 +12,14 @@ export const Product = ({ product }) => {
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className="product-title">
             <strong>{name}</strong>
           </Card.Title>
         </Link>
+
+        <Card.Text as="div">
+          <Rating rating={rating} text={`${numReviews} reviews`} />
+        </Card.Text>
 
         <Card.Text as="h3">${price}</Card.Text>
       </Card.Body>
