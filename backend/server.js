fa935a9989d664b51ff7f5c4648e34fe2b36c349
Sercan <sercansimsek44@gmail.com>
2026-08-27 14:53:12 +0200

    Data served from the server, frontend fetch with axios - response comes from the server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import products from "./data/products.js";
 
-const PORT = 5000;
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
