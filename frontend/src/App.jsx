c28332d93b837a1ad6d14084f783126eb9d9e103
Sercan <sercansimsek44@gmail.com>
2026-08-27 10:13:58 +0200

    Routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,17 @@ import { Header } from "./components/Header";
 import { Container } from "react-bootstrap";
 import { HomeScreen } from "./screens/HomeScreen";
 
+import { Route, Routes } from "react-router-dom";
+
 export const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,19 +1,20 @@
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const Product = ({ product }) => {
   const { image, name, price } = product;
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={image} variant="top" />
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as="h3">${price}</Card.Text>
       </Card.Body>
