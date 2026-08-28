b224e0dbd3ff1d1f9ed2356d53e098cdb5c2d061
Sercan <sercansimsek44@gmail.com>
2026-08-28 11:40:16 +0200

    seeder.js created

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcyrpt from "bcryptjs";
+
+export const users = [
+  {
+    name: "Admin User",
+    email: "admin@email.com",
+    password: bcyrpt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+
+  {
+    name: "John Doe",
+    email: "john@email.com",
+    password: bcyrpt.hashSync("123456", 10),
+    isAdmin: false,
+  },
+
+  {
+    name: "Jane Doe",
+    email: "jane@email.com",
+    password: bcyrpt.hashSync("123456", 10),
+    isAdmin: false,
+  },
+];
